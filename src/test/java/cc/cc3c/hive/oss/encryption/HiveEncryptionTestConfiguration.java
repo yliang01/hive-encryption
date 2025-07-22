08776f005a2844ede2cdfa2d2127ceb5d2d05ad9
YLiang <yliang01@cc3c.cc>
2025-07-22 16:34:06 +0800

    upgrade version

--- src/test/java/cc/cc3c/hive/oss/encryption/HiveEncryptionTestConfiguration.java
+++
@@ -1,7 +0,0 @@
-package cc.cc3c.hive.oss.encryption;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class HiveEncryptionTestConfiguration {
-}
